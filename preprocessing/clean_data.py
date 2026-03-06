f8bdab014a0a029e1cabef86edbb10a9323956c1
Megha Santhosh <spmegha27@gmail.com>
2026-03-06 17:05:21 +0530

    preprocessing

--- preprocessing/clean_data.py
+++ preprocessing/clean_data.py
@@ -2,22 +2,22 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 # Load raw data
-df = pd.read_csv("data/labeled_dataset.csv")
+df = pd.read_csv("data/student_dataset.csv")
 
 print("Original shape:", df.shape)
 
-# 1. Drop ID column
+# Drop ID column
 if 'Register Number' in df.columns:
     df.drop(columns=['Register Number'], inplace=True)
 
-# 2. Handle missing values
+#  Handle missing values
 for col in df.select_dtypes(include='number'):
     df[col].fillna(df[col].median(), inplace=True)
 
 for col in df.select_dtypes(include='object'):
     df[col].fillna(df[col].mode()[0], inplace=True)
 
-# 3. Encode binary categorical columns
+#  Encode binary categorical columns
 le = LabelEncoder()
 
 binary_cols = [
@@ -30,7 +30,7 @@ for col in binary_cols:
     if col in df.columns:
         df[col] = le.fit_transform(df[col])
 
-# 4. Ordinal encoding
+#  Ordinal encoding
 ordinal_map = {'Low': 0, 'Medium': 1, 'High': 2}
 
 ordinal_cols = [
@@ -43,7 +43,7 @@ for col in ordinal_cols:
     if col in df.columns:
         df[col] = df[col].map(ordinal_map)
 
-# 5. One-hot encode device column
+#  One-hot encode device column
 if 'Device used for studing' in df.columns:
     df = pd.get_dummies(df, columns=['Device used for studing'], drop_first=True)
 
